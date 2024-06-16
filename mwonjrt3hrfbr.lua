5040602afeeb842e8c9f8d1d343841654ad04805
Kurus <103210095+kurusyes@users.noreply.github.com>
2024-06-16 19:43:30 +0300

    mwonjrt3hrfbr.lua

---
+++ mwonjrt3hrfbr.lua
@@ -0,0 +1,5 @@
+return {
+	"nijnferjiht83ur2948ru27rhweiufh8rjh28eg8dnhsifyh8rh29erh",
+	"uy47ry92heryqydhsogj7ry28rujsfhsjlfuprfjw89r3y9tekgjeiojropd",
+	"oreol.xyz.3ui05jiufbeuyt30u289rjsejkfh0drk230rh0vksoifuwrk2i0eu2r90-fj"
+}
